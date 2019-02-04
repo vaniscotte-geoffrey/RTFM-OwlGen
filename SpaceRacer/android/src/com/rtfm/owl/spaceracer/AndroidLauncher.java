3292bbab91f3f0b9d5b0691eb1f3dc67f387d2cf
AymegikeLewok <aymeric.henouille@gmail.com>
2019-02-04 09:46:22 +0100

    setup SpaceRacer Project

---
+++ SpaceRacer/android/src/com/rtfm/owl/spaceracer/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rtfm.owl.spaceracer;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rtfm.owl.spaceracer.SpaceRacer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceRacer(), config);
+	}
+}
